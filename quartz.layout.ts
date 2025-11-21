ea322530b298edc5560765e9d19d9d0556927ae6
Chase Ward <wardcp11@gmail.com>
2025-11-21 12:02:53 -0600

    Quartz sync: Nov 21, 2025, 12:02 PM

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -8,7 +8,7 @@ export const sharedPageComponents: SharedLayout = {
   afterBody: [],
   footer: Component.Footer({
     links: {
-      My GitHub: "https://github.com/wardcp11",
+      GitHub: "https://github.com/wardcp11",
     },
   }),
 }
